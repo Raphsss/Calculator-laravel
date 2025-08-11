1e3223b822c744afc8cb06c8790984f771d08c04
Raphael Lucas <rafaoliveira2009@outlook.com>
2025-08-10 23:44:16 -0300

    Navbar atualizada

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,18 +1,15 @@
 <div class="flex h-screen bg-gray-100">
 
-        <!-- Sidebar - fixa à esquerda, ocupa toda a altura e é responsiva -->
         <aside class="bg-gray-800 text-white w-64 space-y-6 py-7 px-2 fixed inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
             
-            <!-- Título ou logo da sidebar -->
             <a href="#" class="text-white text-2xl font-extrabold flex items-center px-4">
-                Minha Sidebar
+                Atividades
             </a>
 
             <!-- Navegação principal -->
             <nav>
                 <ul class="space-y-2">
                     <li>
-                        <!-- Link com efeito hover e padding -->
                         <a href="{{ route('formCalc') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-700 hover:text-white">
                             Calculadora
                         </a>
